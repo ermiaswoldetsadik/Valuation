34b3f7c82d17a938da935ebbfd4bca70b3861c3a
Petros Shumie <shumie.peter@gmail.com>
2020-02-11 11:07:41 -0500

    Initalized mongoose for database connection to mongoDB, created schema variable for User data to be filed into the mongoDB. Exported the file to be used in other files.

--- models/user.js
+++ models/user.js
@@ -1,20 +1,25 @@
+// Initialize mongoose by requiring the mongoose dependency 
 const mongoose = require("mongoose");
+
+// Create initial mongoose schema
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
-  uid: { 
-    type: String,
-    unique: true
-  },
-  name: { 
-    type: String
-  },
-  email: String,
-  image: String,
-  gender: String,
-  dateJoined: { type: Date }
-});
+// Setup schema for mongoDB for users who are evaluating items 
+  const UserSchema = new Schema({
+    uid: {
+      type: String,
+      unique: true
+    },
+    name: {
+      type: String
+    },
+    email: String,
+    image: String,
+    gender: String,
+    dateJoined: { type: Date }
+  });
 
-const User = mongoose.model("User", UserSchema);
+  // Set user variable equal to the user schema defined in the model
+   const User = mongoose.model("User", UserSchema);
 
-module.exports = User;
+   module.exports = User;
