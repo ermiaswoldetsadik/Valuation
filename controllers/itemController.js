d7334b05d2ecff5b7af9bec08d4773dbff3ff83a
Petros Shumie <shumie.peter@gmail.com>
2020-02-11 12:32:57 -0500

    Refactored and addded description of api methods used for crud functionality on for items in the databse.

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,20 +1,23 @@
+// Import db schema from models folder
 const db = require("../models/");
 
-// Defining methods for the item
+// API methods for item schema, query for entire database
 module.exports = {
   findAll: function(req, res) {
     db.Item
-      .find(req.query)
-      .sort({ date: -1 })
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
+    .find(req.query)
+    .sort({ date: -1 })
+    .then(dbModel => res.json(dbModel))
+    .catch(err => res.status(422).json(err));
   },
+// API method for querying the database by an items unique ID established in the item model
   findById: function(req, res) {
     db.Item
       .findById(req.params.id)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
+  // API method for querying the database by an items Owner
   findByOwner: function(req, res) {
     console.log(req.params)
     db.Item
@@ -22,18 +25,21 @@ module.exports = {
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
+  // API method for creating new items to placed into the db
   create: function(req, res) {
     db.Item
       .create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
+  // API method for updating existing data in the db
   update: function(req, res) {
     db.Item
       .findOneAndUpdate({ _id: req.params.id }, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
+  // API method for removing existing data in the db
   remove: function(req, res) {
     db.Item
       .findById({ _id: req.params.id })
