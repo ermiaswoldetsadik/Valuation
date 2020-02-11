d97c6c8a0086fc5c88f15f02a315911ec4fa07c9
Petros Shumie <shumie.peter@gmail.com>
2020-02-11 10:50:38 -0500

    Imported mongoose to connect to the MONGODB, set the schema of the data entering the database, exported the module for use in other files.

--- models/review.js
+++ models/review.js
@@ -1,6 +1,9 @@
+// Import mongoose for database connection
 const mongoose = require("mongoose");
+// Set Schema variable equal to the schema introduced to the mongo db via mongoose
 const Schema = mongoose.Schema;
 
+// Set ReviewSchema for review data to be imported into the mongodb
 const ReviewSchema = new Schema({
   reviewDate: { type: Date },
   quality: { type: Number },
@@ -11,6 +14,8 @@ const ReviewSchema = new Schema({
   notes: { type: String }
 });
 
+// Set review variable equal to the database model for mongoose
 const Review = mongoose.model("Review", ReviewSchema);
 
+// Set the Review file to be exported for other files to use
 module.exports = Review;
