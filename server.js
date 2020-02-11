0138c55f4b8b89a14ac300501a2f3bbd69394b55
Petros Shumie <shumie.peter@gmail.com>
2020-02-11 10:31:08 -0500

    Refactored code to clean up and errors in syntax. Imported required dependencies for app functionality initalize server to use express for route handling, connect to the MONGO DB via mongoose, start listening for request to acess the server.

--- server.js
+++ server.js
@@ -1,30 +1,32 @@
+// Import dependencies for application functionality
 const express = require ("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const routes = require("./routes"); 
+const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 const path = require("path");
-const fs = require('fs');
-const multer = require('multer');
-const axios = require('axios');
+const fs = require("fs");
+const multer = require("multer");
+const axios = require("axios");;
 
-// configures body parser for AJAX requests
+//Initialize Body-parser for JSON data
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// serves up static assets
+//Folder for static data
 app.use(express.static("client/build"));
 
-// adds the routes
-app.use(routes); 
+// Use routes folder for express api routes
+app.use(routes);
 
-// sets up promises with mongoose
+// Initialize Mongoose connection to MONGO DB
 mongoose.Promise = global.Promise;
-// connects to the mongo db
-mongoose.connect( process.env.MONGODB_URI || "mongodb://<items>:<valuation123>@ds035557.mlab.com:35557/heroku_6lwhkp9d");
 
-// starts the API server
+// Connect to the MongoDB via Mongoose
+mongoose.connect( process.env.MONGODB_URI || "mongodb://<items>:<valuation123>@ds035557.mlab.com:35557/heroku_6lwhkp9d")
+
+// Start listening on PORT for server
 app.listen(PORT, function() {
     console.log(`🌎  ==> API Server now listening on PORT ${PORT}.`);
-});
+})
