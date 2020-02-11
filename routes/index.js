2d223b96c7c534997024a3aa0f33cb10d676f29b
Petros Shumie <shumie.peter@gmail.com>
2020-02-11 10:37:33 -0500

    Added imports for api router to be used to route between request from the client and the server.

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
+// Import dependencies for api routes to connect via express and node module path
 const path = require("path");
 const router = require("express").Router();
 const apiRoutes = require("./api");
 
-// API routes
+// API routes 
 router.use("/api", apiRoutes);
 
-
-// if no API routess are hit, send the react app
+// If no API routes are hit, send the react app
 router.use(function(req, res) {
     res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
