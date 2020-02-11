dd68cc8805b9ba8489aff7d42999f8bfac971d1a
Petros Shumie <shumie.peter@gmail.com>
2020-02-11 12:38:59 -0500

    Added controller methods for items in the db along with reviews of those items. Also updated the constroller method for uploading an image and writing its meta data to our mongo db via multer.

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,6 +1,6 @@
 const db = require("../models/");
 
-// Defining methods for the item
+// API methods for finding all data in the database that stores review data
 module.exports = {
   findAll: function(req, res) {
     db.Review
@@ -9,30 +9,35 @@ module.exports = {
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
+  // API method for finding review data by unique id
   findById: function(req, res) {
     db.Review
       .findById(req.params.id)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
+  // API method for finding reviewed Item data by id
   findByItemId: function(req, res) {
     db.Review
       .find(req.params)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
+  // API method for creating reviews
   create: function(req, res) {
     db.Review
       .create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
+  // API method for updating an existing review
   update: function(req, res) {
     db.Review
       .findOneAndUpdate({ _id: req.params.id }, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
+  // API method for removing a review from the database
   remove: function(req, res) {
     db.Review
       .findById({ _id: req.params.id })
--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -1,9 +1,11 @@
+// Dependencies required for data uploading 
 const db = require("../models/");
 const path = require("path");
 const fs = require('fs');
 const multer = require('multer');
 
-
+// Multer allows for adding files from a users local storage to the database
+//This upload controller allows someone to update the database with an image
 const storage = multer.diskStorage({
   destination:'./client/public/uploads',
   filename(req, file, cb) {
@@ -12,9 +14,11 @@ const storage = multer.diskStorage({
   },
 });
 
+// Upload is set the the variable multer which is a storage method
 const upload = multer({ storage });
 
 // Defining methods for the item
+// Meta data from the existing image is stored into the database 
 module.exports = {
   create: function(req, res) {
     const file = req.file; // file passed from client
