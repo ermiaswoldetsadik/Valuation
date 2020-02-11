28dd62ec34f497278c0bcb2fa263ecddf74b77c8
Petros Shumie <shumie.peter@gmail.com>
2020-02-11 10:44:15 -0500

    Imported mongoose for database connection, created item schema for data once pulled into the MongoDB, set the data to a variable equal to the Item Schema and exported the module to be used in other files.

--- models/item.js
+++ models/item.js
@@ -1,8 +1,10 @@
+// Imports for connecting to Mongoose
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+
+// Create schema for mongoose database collection to store item data
 
 const ItemSchema = new Schema({
-  img: { type: String},
+  img: { type: String },
   dateUploaded: { type: Date, default: Date.now },
   description: { type: String },
   name: { type: String },
@@ -10,6 +12,8 @@ const ItemSchema = new Schema({
   ownerId: { type: String }
 });
 
+// Set the Item variable equal to the mongoose Item Schema
 const Item = mongoose.model("Item", ItemSchema);
 
+// Export the module to be used in other files
 module.exports = Item;
